a2442cab8b7054fd9907c406231f1c78079a143c
Birju Shah <birju@tweakbits.in>
2026-04-21 15:36:44 +0530

    feat(test-generator-registry): barrel exports full public surface

--- packages/test-generator-registry/src/index.ts
+++ packages/test-generator-registry/src/index.ts
@@ -1,1 +1,21 @@
-export {};
+export { TestGeneratorRegistry } from "./registry.js";
+export { HumanBaselineStore } from "./baseline-store.js";
+export {
+  DriftDetector,
+  hashActivationBody,
+  CalibrationEntrySchema,
+  CalibrationFileSchema
+} from "./drift.js";
+export type { CalibrationEntry, CalibrationFile, DriftReport } from "./drift.js";
+export { invokeGenerator } from "./invoker.js";
+export type { GeneratorResult, InvokeGeneratorInput } from "./invoker.js";
+export { isProtectedTarget, protectedKindOf } from "./protected.js";
+export type { ProtectedKind } from "./protected.js";
+export { BaselineFileSchema, BaselineAssertionSchema } from "./baseline-schema.js";
+export type { BaselineFile, BaselineAssertion } from "./baseline-schema.js";
+export {
+  NoGeneratorForKindError,
+  BaselineMissingError,
+  BaselineFileParseError,
+  DriftExceededError
+} from "./errors.js";
