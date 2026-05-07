b61c9216e058992250f9dbfc5a7bd5b9a5da4926
Birju Shah <birju@tweakbits.in>
2026-05-07 18:01:59 +0530

    feat(role-developer): export sandbox-context-registry helpers

--- packages/role-developer/src/index.ts
+++ packages/role-developer/src/index.ts
@@ -5,3 +5,9 @@ export { anthropicPass, DEVELOPER_ANTHROPIC_MODEL, type AnthropicPassInput } fro
 export { googlePass, DEVELOPER_GOOGLE_MODEL, type GooglePassInput } from "./google-pass.js";
 export { reviewerVote, DEVELOPER_REVIEWER_MODEL, type ReviewerInput } from "./reviewer-vote.js";
 export { DeveloperRoleError, SkillMissingError, BothProvidersFailedError, ReviewerFailedError } from "./errors.js";
+export {
+  getSandboxContextPrompt,
+  listAvailableTemplates,
+  DEFAULT_TEMPLATE_NAME
+} from "./sandbox-context-registry.js";
+export { getSandboxContextPromptFor } from "./assemble-prompt.js";
