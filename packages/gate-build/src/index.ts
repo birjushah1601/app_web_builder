82fc5e5c14adbe54aed80d46f884572a03a118eb
Birju Shah <birju@tweakbits.in>
2026-05-18 07:59:38 +0000

    feat(gate-build): public export surface

---
+++ packages/gate-build/src/index.ts
@@ -0,0 +1,21 @@
+export {
+  BuildReportSchema,
+  BuildErrorSchema,
+  BuildErrorKindSchema,
+  type BuildReport,
+  type BuildError,
+  type BuildErrorKind
+} from "./schema.js";
+
+export {
+  type SandboxExec,
+  type RunCommandInput,
+  type RunCommandResult,
+  SandboxUnreachableError
+} from "./sandbox-exec.js";
+
+export { BUILD_COMMANDS, type BuildCommand, type ParserId, type KnownTemplate } from "./commands.js";
+
+export { parseTscOutput, parsePyrightJson } from "./parse.js";
+
+export { BuildGateRole, type BuildGateRoleOptions } from "./role.js";
